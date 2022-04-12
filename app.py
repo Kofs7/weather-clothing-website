cedd656743208de904f60612cdef642314921f5f
Yoofi <yofs@techexchange.in>
2022-04-12 02:12:37 +0000

    build routes

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, session
+from flask import render_template, redirect 
+from flask import url_for, flash
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/index')
+def home():
+    return "Home Page"
+
+@app.route('/rack')
+def rack():
+    return "rack page"
+
+@app.route('/combos')
+def combos():
+    return 'combos'
+
+@app.route('/login')
+def login():
+    return "login"
+
+@app.route('/logout')
+def logout():
+    return 'logout'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
