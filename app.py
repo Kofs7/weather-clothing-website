8502566a88324c5cb97d4be3ba4d02cc35871885
brianP1201 <42940710+brianP1201@users.noreply.github.com>
2022-04-29 13:48:03 -0400

    Added current_season function to filter clothes selection

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, session
 from flask import render_template, redirect 
 from flask import url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
-app.secret_key = 'tH1x93H??s1Zow_#~2'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/items.sqlite'
@@ -12,55 +12,76 @@ app.config['SQLALCHEMY_BINDS'] = {
     'users_saved': 'sqlite:///databases/user_saved.sqlite'
 }
 db = SQLAlchemy(app)
+api_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Washington%2C%20DC/today?unitGroup=us&key=K3WUKAZ4638HASZFK2UBXBU3X&contentType=json"
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     style = db.Column(db.String(20))
     item_type = db.Column(db.String(50))
-    weather = db.Column(db.String(50))
+    season = db.Column(db.String(50))
     image = db.Column(db.String(100))
 
-    def __init__(self, style, item_type, weather, image):
+    def __init__(self, style, item_type, season, image):
         self.style = style
         self.item_type = item_type
-        self.weather = weather
+        self.season = season
         self.image = image
-
 class Saved(db.Model):
     __bind_key__ = 'users_saved'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(50))
     item_type = db.Column(db.String(50))
-    weather = db.Column(db.String(50))
+    season = db.Column(db.String(50))
     image = db.Column(db.String(100))
 
-    def __init__(self, user, style, item_type, weather, image):
+    def __init__(self, user, style, item_type, season, image):
         self.user = user
         self.style = style
         self.item_type = item_type
-        self.weather = weather
+        self.season = season
         self.image = image
 
+def current_season():
+    try: 
+        ResultBytes = urllib.request.urlopen(api_URL)
+        # Parse the results as JSON
+        jsonData = json.loads(ResultBytes.decode('utf-8'))
+    except urllib.error.HTTPError  as e:
+        ErrorInfo= e.read().decode() 
+        print('Error code: ', e.code, ErrorInfo)
+        sys.exit()
+    except  urllib.error.URLError as e:
+        ErrorInfo= e.read().decode() 
+        print('Error code: ', e.code,ErrorInfo)
+        sys.exit()
+
+    if jsonData[currentConditions][temp] <= 50.9:
+        return "winter"
+    elif jsonData[currentConditions][temp] >= 51.0 and jsonData[currentConditions][temp] <= 70.9:
+        return "fall"
+    elif jsonData[currentConditions][temp] >= 71.0 and jsonData[currentConditions][temp] <= 84.9:
+        return "spring"
+    else:
+        return "summer"
+    
+
+
 @app.route('/')
-@app.route('/index', methods=["POST", "GET"])
+@app.route('/index')
 def home():
-    # if 'user' in session:
-    #     session['user'] = request.form['name']
-    #     user = session['user']
-    #     return render_template('index.html', name=user)
     return render_template('index.html')
 
 @app.route('/rack', methods=['GET', 'POST'])
 def rack():
     all_items = Item.query.all()
-    # filters = Item.query.filter_by(style='casual', item_type='top')
+    #filters = Item.query.filter_by(style='casual', item_type='top')
 
-    # if request.method == 'POST':  
-    #     filter_dict = {'item_type': request.form['filter-clothes'],
-    #                    'style': request.form['filter-style'],
-    #                    'weather': request.form['filter-weather']   
-    #                   }      
-       
+    if request.method == 'POST':  
+        filter_dict = {'item_type': request.form['filter-clothes'],
+                       'style': request.form['filter-style'],
+                       'season': request.form['filter-season']   
+                      }      
+        
     return render_template('rack.html', items=all_items)
 
 @app.route('/combos')
@@ -71,18 +92,6 @@ def combos():
     # shoes = items['shoes']
     
     return render_template('combo.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['name']
-        session['user'] = user
-
-        return redirect(url_for('home'))
-    else:
-        if 'user' in session:
-            redirect(url_for('home')) 
-        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
